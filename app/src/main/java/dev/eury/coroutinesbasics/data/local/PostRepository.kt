bcc01a338f4139e60be01b0abed04fbfe22205cd
Eury Perez <ebeltre@beachbody.com>
2022-06-22 02:38:18 +0200

    cleaning upo

--- app/src/main/java/dev/eury/coroutinesbasics/data/local/PostRepository.kt
+++ app/src/main/java/dev/eury/coroutinesbasics/data/local/PostRepository.kt
@@ -1,12 +1,7 @@
 package dev.eury.coroutinesbasics.data.local
 
-import dev.eury.coroutinesbasics.data.local.SessionManager
-import dev.eury.coroutinesbasics.data.models.Album
-import dev.eury.coroutinesbasics.data.models.Comment
 import dev.eury.coroutinesbasics.data.models.Post
-import dev.eury.coroutinesbasics.data.models.Todo
 import dev.eury.coroutinesbasics.data.network.API
-import retrofit2.Callback
 
 class PostRepository {
 
